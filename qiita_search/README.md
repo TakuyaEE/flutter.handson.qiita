be6a5767b130f468786336e5857dc1b6afe67fdd
TakuyaIi <takuyaelle@gmail.com>
2024-02-04 12:43:29 +0900

    ✨ ハンズオンの修了

--- qiita_search/README.md
+++ qiita_search/README.md
@@ -1,16 +1,11 @@
-# qiita_search
+# 以下のハンズオンを実装
 
-A new Flutter project.
+https://zenn.dev/heyhey1028/books/flutter-basics
 
-## Getting Started
+## .env ファイルの作成が必要
 
-This project is a starting point for a Flutter application.
+この[URL](https://zenn.dev/heyhey1028/books/flutter-basics/viewer/hands_on_1)を参考にアクセストークンを取得してプロジェクト直下に.env ファイルを作成し、記載
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+```
+QIITA_ACCESS_TOKEN=b07522f894e9###################464
+```
--- qiita_search/lib/screens/search_screen.dart
+++ qiita_search/lib/screens/search_screen.dart
@@ -3,6 +3,8 @@ import "dart:convert";
 import "package:http/http.dart" as http;
 import "package:flutter_dotenv/flutter_dotenv.dart";
 import "package:qiita_search/models/article.dart";
+import "package:qiita_search/models/user.dart";
+import "package:qiita_search/widgets/article_container.dart";
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -12,14 +14,41 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<Article> articles = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Qiita Search'),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: const Text('Qiita Search'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 36),
+              child: TextField(
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+                decoration: const InputDecoration(
+                  hintText: "検索ワードを入力してください",
+                ),
+                onSubmitted: (String value) async {
+                  final results = await searchQiita(value);
+                  setState(() => articles = results);
+                },
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: articles
+                    .map((article) => ArticleContainer(article: article))
+                    .toList(),
+              ),
+            ),
+          ],
+        ));
   }
 }
 
